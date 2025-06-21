07a4f41dd7db7fadc5c2922a8105a5b7b853bbc3
Syed Nisar Aoun <nisar.aoun123@gmail.com>
2025-06-21 12:01:45 +0500

    Add Render domain to ALLOWED_HOSTS

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)kvld291fe!3wiz#tb24(wb#pyejp43nv*6v)z%)r^!&tny=3e
 # SECURITY WARNING: don't run with debug turned on in production
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.14', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
