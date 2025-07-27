be95f6e181717b332e1295b995cd7820dd37d826
Syed Nisar Aoun <nisar.aoun123@gmail.com>
2025-07-27 16:58:44 +0500

    Add first_name and last_name fields to CustomUser

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,11 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-from django.utils import timezone
+from django.contrib.auth.models import BaseUserManager
 
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         if not email:
-            raise ValueError("The Email field must be set")
+            raise ValueError('Email is required')
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
@@ -19,18 +19,18 @@ class CustomUserManager(BaseUserManager):
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
+    totp_secret = models.CharField(max_length=32, blank=True, null=True)
+
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    date_joined = models.DateTimeField(default=timezone.now)
-    totp_secret = models.CharField(max_length=32, blank=True, null=True)
-    otp_verified = models.BooleanField(default=False)
-    qr_code_image = models.ImageField(upload_to='qr_codes/', blank=True, null=True)
-
 
     objects = CustomUserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
 
     def __str__(self):
         return self.email
