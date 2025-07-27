545015e1b9b6e9416cbd263c8acbf5977195fc6d
Syed Nisar Aoun <nisar.aoun123@gmail.com>
2025-07-27 17:57:23 +0500

    Fix: Ensure first_name and last_name are saved in signup

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -4,15 +4,17 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class CustomUserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-
     class Meta:
         model = User
-        fields = ('email', 'password')
+        fields = ['id', 'email', 'password', 'first_name', 'last_name']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        return User.objects.create_user(
             email=validated_data['email'],
-            password=validated_data['password']
+            password=validated_data['password'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', '')
         )
-        return user
